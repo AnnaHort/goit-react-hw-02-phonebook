635f6413bf3ec253d440d11f6fbde92e4e7b6acd
AnnaHort <annahort3d@gmail.com>
2023-08-12 15:12:41 +0300

    реалізовано 1 та 2 частина дз

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { QuizForm } from './QuizForm/QuizForm';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  };
+
+  addContact  = (newContact) => {
+  this.setState(prevState => {
+    return {
+      contacts: [...prevState.contacts, newContact]
+    }
+  })
+}
+
+  render() {
+    return (
+      <>
+        <GlobalStyle/>
+        <h2>Phonebook</h2>
+        <QuizForm onAdd={this.addContact }/>
+        <h2>Contacts</h2>
+        <ul>
+          <li>Eden Clements: 645-78-78</li>
+          <li>Hermione Kline: 789-45-67</li>
+          <li>Rosie Simpson: 498-79-15</li>
+        {this.state.contacts.map((contact) => (
+            <li key={contact.id}>
+              {contact.name}: {contact.contacts}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
